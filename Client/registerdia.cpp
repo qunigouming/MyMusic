d26b09347cdf2d6da07587b3a3aa8c345c96469b
qunigoumin <3120248848@qq.com>
2022-09-20 09:45:57 +0800

    再次优化注册界面ui

--- Client/registerdia.cpp
+++ Client/registerdia.cpp
@@ -1,15 +1,51 @@
 #include "registerdia.h"
 #include "ui_registerdia.h"
 
+#include <QMouseEvent>
+
 RegisterDia::RegisterDia(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::RegisterDia)
 {
     ui->setupUi(this);
     setWindowFlag(Qt::FramelessWindowHint);
+    setModal(true);         //设置窗口为模态显示
+    ui->userNameLine->setFocus();                   //设置焦点
+    ui->titleWidget->installEventFilter(this);     //给标题栏安装事件过滤器
 }
 
 RegisterDia::~RegisterDia()
 {
     delete ui;
+}
+
+void RegisterDia::on_exitWinodwBtn_clicked()
+{
+    close();
+}
+
+bool RegisterDia::eventFilter(QObject *watched, QEvent *event)
+{
+    //捕获标题窗口
+    if (watched == ui->titleWidget) {
+        if (event->type() == QEvent::MouseButtonPress) {
+            QMouseEvent *e = (QMouseEvent*)event;
+            if (e->buttons() == Qt::LeftButton)
+                dragPosition = e->globalPos();      //记录鼠标全局位置
+        }
+        if (event->type() == QEvent::MouseMove) {
+            QMouseEvent *e = (QMouseEvent*)event;
+            if (e->buttons() == Qt::LeftButton) {
+                QPoint tempPos = e->globalPos() - dragPosition;
+                move(this->pos() + tempPos);
+                dragPosition = e->globalPos();
+            }
+        }
+    }
+    return QDialog::eventFilter(watched,event);
+}
+
+void RegisterDia::on_QuitBtn_clicked()
+{
+    close();
 }
--- Client/registerdia.h
+++ Client/registerdia.h
@@ -2,6 +2,7 @@
 #define REGISTERDIA_H
 
 #include <QDialog>
+#include "tcpsocket.h"
 
 namespace Ui {
 class RegisterDia;
@@ -15,8 +16,19 @@ public:
     explicit RegisterDia(QWidget *parent = 0);
     ~RegisterDia();
 
+private slots:
+    void on_exitWinodwBtn_clicked();
+
+    void on_QuitBtn_clicked();
+
+protected:
+    bool eventFilter(QObject *watched, QEvent *event);      //事件过滤器
+
 private:
     Ui::RegisterDia *ui;
+
+    //标题栏
+    QPoint dragPosition;
 };
 
 #endif // REGISTERDIA_H
