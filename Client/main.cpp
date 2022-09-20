15ba794159dcd40592b0f0797c3d27342228d869
qunigoumin <3120248848@qq.com>
2022-09-20 19:59:53 +0800

    再次优化ui

--- Client/main.cpp
+++ Client/main.cpp
@@ -6,8 +6,8 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    //Login::getInstance().show();
-    RegisterDia d;
-    d.show();
+    Login::getInstance().show();
+//    RegisterDia d;
+//    d.show();
     return a.exec();
 }
